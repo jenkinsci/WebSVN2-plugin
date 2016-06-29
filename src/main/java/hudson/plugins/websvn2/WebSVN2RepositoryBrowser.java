9107496c098bd7b3ee80b4bc7c9670d578c3f3b7
Thorko <thorko@thorko.de>
2016-06-29 10:26:05 +0200

    fix websvn2 plugin to work with websvn 2.3.3

--- src/main/java/hudson/plugins/websvn2/WebSVN2RepositoryBrowser.java
+++ src/main/java/hudson/plugins/websvn2/WebSVN2RepositoryBrowser.java
@@ -23,153 +23,128 @@
  */
 package hudson.plugins.websvn2;
 
-import hudson.Extension;
-import hudson.model.Descriptor;
-import hudson.scm.RepositoryBrowser;
-import hudson.scm.SubversionChangeLogSet.LogEntry;
-import hudson.scm.SubversionChangeLogSet.Path;
-import hudson.scm.SubversionRepositoryBrowser;
-import hudson.util.FormValidation;
-import net.sf.json.JSONObject;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.DataBoundConstructor;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.scm.RepositoryBrowser;
+import hudson.scm.SubversionRepositoryBrowser;
+import hudson.scm.SubversionChangeLogSet.LogEntry;
+import hudson.scm.SubversionChangeLogSet.Path;
+import hudson.util.FormValidation;
+import net.sf.json.JSONObject;
 
 /**
- * {@link SubversionRepositoryBrowser} that produces links to http://www.websvn.info/ for SVN
- * compatible with Version 2.3.1 of WebSVN.
+ * {@link SubversionRepositoryBrowser} that produces links to http://www.websvn.info/ for SVN compatible with Version
+ * 2.3.1 of WebSVN.
  *
- * @author Andreas Mandel, based on ViewVC plugin by Mike Salnikov, based on Polarion plug-in by Jonny Wray
+ * @author Christian Möller, based on work by Andreas Mandel, based on ViewVC plugin by Mike Salnikov, based on Polarion
+ *         plug-in by Jonny Wray
+ * @version $Revision: 83023 $ ($Date: 2016-06-16 15:48:53 +0200 (Do, 16 Jun 2016) $) by $Author: christian $
  */
 public class WebSVN2RepositoryBrowser extends SubversionRepositoryBrowser {
 
-    // alternative: https://server/websvn/comp.php?repname=rep&compare[]=/@2222&compare[]=/@2225
-    private static final String CHANGE_SET_FORMAT
-        = "revision.php?%1srev=%2d";
-        
-    private static final String DIFF_FORMAT
-        = "diff.php?%1spath=%2s&rev=%3d";
-        
-    // alternative: "blame.php?%1spath=%2s&rev=%3d";
-    private static final String FILE_FORMAT
-        = "filedetails.php?%1spath=%2s&rev=%3d";
-        
-    private static final Pattern URL_PATTERN
-        = Pattern.compile(
-            "(.*/)(revision|diff|comp|filedetails|listing|blame|dl|log)"
-            + "\\.php([^?]*)\\?(repname=([^&]*))?(.*)");
-    private static final int URL_PATTERN_BASE_URL_GROUP = 1;
-    private static final int URL_PATTERN_REPNAME_GROUP = 4;
+  private static final long serialVersionUID = 6649912705777392101L;
 
-    public final URL url;
-    private final URL baseUrl;
-    private final String repname;
+  private static final String CHANGE_SET_FORMAT = "%1s?op=revision&rev=%2d";
+  private static final String DIFF_FORMAT = "%1s/%2s?op=diff&rev=%3d";
+  private static final String FILE_FORMAT = "%1s/%2s?rev=%3d";
+  private static final Pattern WEBSVN_URL_PATTERN = Pattern.compile("(.*/wsvn)(/[^/]*)?/?");
+  private static final int URL_PATTERN_BASE_URL_GROUP = 1;
+  private static final int URL_PATTERN_REPNAME_GROUP = 2;
 
-    @DataBoundConstructor
-    public WebSVN2RepositoryBrowser(URL url) throws MalformedURLException {
-        final Matcher webSVNurl = URL_PATTERN.matcher(url.toString());
-        this.url = url;
-        if (!webSVNurl.matches()) {
-            this.repname = "";
-            this.baseUrl = url;
-        } else {
-            this.baseUrl = new URL(webSVNurl.group(URL_PATTERN_BASE_URL_GROUP));
-            this.repname = webSVNurl.group(URL_PATTERN_REPNAME_GROUP) + "&";
-        }
+  public final URL url;
+  private final URL baseUrl;
+  private final String nameOfSvnRepository;
+
+  @DataBoundConstructor
+  public WebSVN2RepositoryBrowser(final URL url) throws MalformedURLException {
+    final Matcher webSVNurl = WEBSVN_URL_PATTERN.matcher(url.toString());
+    if (webSVNurl.matches()) {
+      this.url = url;
+      this.baseUrl = new URL(webSVNurl.group(URL_PATTERN_BASE_URL_GROUP) + "/");
+      this.nameOfSvnRepository = webSVNurl.group(URL_PATTERN_REPNAME_GROUP).substring("/".length());
+    } else {
+      throw new MalformedURLException(
+          "Please set a WebSVN2 url in the form http://<i>server</i>/<i>optional-path</i>/wsvn/<i>Name-of-SVN-repo</i>.");
     }
+  }
 
-    public String getRepname() {
-        return this.repname;
-    }
+  public String getRepname() {
+    return nameOfSvnRepository;
+  }
 
-    @Override
-    public URL getDiffLink(Path path) throws IOException {
-        return new URL(
-            this.baseUrl,
-            String.format(DIFF_FORMAT,
-                getRepname(),
-                URLEncoder.encode(path.getValue(), "UTF-8"),
-                path.getLogEntry().getRevision()));
-    }
+  @Override
+  public URL getDiffLink(final Path path) throws IOException {
+    return new URL(baseUrl, String.format(DIFF_FORMAT, nameOfSvnRepository, URLEncoder.encode(path.getValue(), "UTF-8"),
+        path.getLogEntry().getRevision()));
+  }
 
-    @Override
-    public URL getFileLink(Path path) throws IOException {
-        // TODO: If this is a dir we should rather use listing
-    	return new URL(
-            this.baseUrl,
-            String.format(FILE_FORMAT,
-                getRepname(),
-                URLEncoder.encode(path.getValue(), "UTF-8"),
-                path.getLogEntry().getRevision()));
+  @Override
+  public URL getFileLink(final Path path) throws IOException {
+    // TODO: If this is a dir we should rather use listing
+    return new URL(baseUrl, String.format(FILE_FORMAT, nameOfSvnRepository, URLEncoder.encode(path.getValue(), "UTF-8"),
+        path.getLogEntry().getRevision()));
+  }
+
+  @Override
+  public URL getChangeSetLink(final LogEntry changeSet) throws IOException {
+    return new URL(baseUrl, String.format(CHANGE_SET_FORMAT, nameOfSvnRepository, changeSet.getRevision()));
+  }
+
+  @Extension
+  public static final class DescriptorImpl extends Descriptor<RepositoryBrowser<?>> {
+
+    public DescriptorImpl() {
+      super(WebSVN2RepositoryBrowser.class);
     }
 
     @Override
-    public URL getChangeSetLink(LogEntry changeSet) throws IOException {
-        return new URL(
-            this.baseUrl,
-            String.format(CHANGE_SET_FORMAT,
-                getRepname(), changeSet.getRevision()));
+    public String getDisplayName() {
+      return "WebSVN2";
     }
 
-    @Extension
-    public static final class DescriptorImpl extends Descriptor<RepositoryBrowser<?>> {
-        public DescriptorImpl() {
-            super(WebSVN2RepositoryBrowser.class);
+    public FormValidation doCheckReposUrl(@QueryParameter final String queryParameterValue) {
+      if (queryParameterValue == null || queryParameterValue.isEmpty()) {
+        return FormValidation.errorWithMarkup(
+            "Please set a WebSVN2 url in the form http://<i>server</i>/<i>optional-path</i>/wsvn/<i>Name-of-SVN-repo</i>.");
+      }
+
+      final Matcher queryParameterAsUrlPatternMatcher = WEBSVN_URL_PATTERN.matcher(queryParameterValue);
+      if (queryParameterAsUrlPatternMatcher.matches()) {
+        try {
+          @SuppressWarnings("unused")
+          final URL syntaxCheckOnly = new URL(queryParameterAsUrlPatternMatcher.group(URL_PATTERN_BASE_URL_GROUP));
+        } catch (final MalformedURLException ex) {
+          return FormValidation.error("The entered url is not accepted: " + ex.getLocalizedMessage());
         }
 
-        public String getDisplayName() {
-            return "WebSVN2";
+        final String nameOfSvnRepository = queryParameterAsUrlPatternMatcher.group(URL_PATTERN_REPNAME_GROUP);
+        if (nameOfSvnRepository == null || nameOfSvnRepository.isEmpty() || nameOfSvnRepository.equals("/")) {
+          return FormValidation.errorWithMarkup(
+              "Please set a WebSVN2 url containing SVN repository in the form http://<i>server</i>/<i>optional-path</i>/wsvn/<i>Name-of-SVN-repo</i>.");
         }
 
-        public FormValidation doCheckReposUrl(@QueryParameter String value) {
-            FormValidation result;
-            final Matcher matcher = URL_PATTERN.matcher(value);
-            if(matcher.matches())
-            {
-                try
-                {
-                    final URL repUrl = new URL(matcher.group(URL_PATTERN_BASE_URL_GROUP));
-                    final String repName = matcher.group(URL_PATTERN_REPNAME_GROUP);
-                    if (repName == null || "".equals(repName))
-                    {   // Go online??
-                        result = FormValidation.okWithMarkup(
-                            "Please set a url including the repname property if needed.");
-                    }
-                    else
-                    {
-                        result = FormValidation.ok();
-                    }
-                }
-                catch (MalformedURLException ex)
-                {
-                    result = FormValidation.error(
-                        "The entered url is not accepted: " + ex.getLocalizedMessage());
-                }
-            }
-            else if ("".equals(value))
-            {
-                result = FormValidation.okWithMarkup(
-                    "Please set a WebSVN url in the form "
-                    + "https://<i>server</i>/websvn/listing.php?repname=<i>rep</i>&path=/trunk/..");
-            }
-            else
-            {
-                result = FormValidation.error(
-                    "Please set a url including the WebSVN php script.");
-            }
-            return result;
-        }
+        return FormValidation.ok();
+      }
 
-        @Override
-        public WebSVN2RepositoryBrowser newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-            return req.bindParameters(WebSVN2RepositoryBrowser.class, "webSVN2.");
-        }
+      return FormValidation.errorWithMarkup(
+          "Please set a WebSVN2 url containing wsvn path as well as SVN repository in the form http://<i>server</i>/<i>optional-path</i>/wsvn/<i>Name-of-SVN-repo</i>.");
     }
+
+    @Override
+    public WebSVN2RepositoryBrowser newInstance(final StaplerRequest staplerRequest, final JSONObject formData)
+        throws FormException {
+      return staplerRequest.bindParameters(WebSVN2RepositoryBrowser.class, "webSVN2.");
+    }
+  }
 }
